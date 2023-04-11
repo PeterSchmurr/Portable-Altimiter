ebcd624d3319bc183c92ec629e42978016617443
Peter Schmurr <schmurrsmail@gmail.com>
2023-04-10 21:32:46 -0400

    add constant for zero button....add initialization pins for LCD

--- portable_altimeter/portable_altimeter.ino
+++ portable_altimeter/portable_altimeter.ino
@@ -3,7 +3,12 @@
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 Adafruit_BME280 bme;  // I2C
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+//LiquidCrystal lcd(7, 8, 9, 10, 11, 12); using the mega board that came with the kit Eric gave me
+  // initialize the LCD library with the GPIO numbers of the interface pins
+LiquidCrystal lcd(19, 23, 18, 17, 16, 15);
+
+const int zeroButton = 4;// input for resetting display to 0 
+
 void setup() {
   Serial.begin(115200);
   lcd.begin(16, 2);
@@ -13,7 +18,7 @@ void setup() {
 //    Serial.println("Could not find a valid BMP280 sensor, check wiring or change I2C address!");
 //    while (1);
 //  }
-  lcd.print("Altitude");
+  //lcd.print("Altitude");
 }
 void loop() {
 lcd.setCursor(0, 1);
@@ -21,5 +26,3 @@ lcd.setCursor(0, 1);
 lcd.print(bme.readAltitude(1021)*3.3);
 delay(2000);                         
 }
-
-// add pushbutton to reset pressure reading to 0
