a3957d8c7dd0a27d2c7c70a562fa62f294021b8c
Peter Schmurr <schmurrsmail@gmail.com>
2023-04-11 14:05:19 -0400

    add variables and pin assignments

--- portable_altimeter/portable_altimeter.ino
+++ portable_altimeter/portable_altimeter.ino
@@ -7,11 +7,17 @@ Adafruit_BME280 bme;  // I2C
   // initialize the LCD library with the GPIO numbers of the interface pins
 LiquidCrystal lcd(19, 23, 18, 17, 16, 15);
 
-const int zeroButton = 4;// input for resetting display to 0 
+const int zeroButton = 4;// input for resetting display to 0
+int altitude; // current reading from bme280
+int zero; // altitude at time of zero calibration 
+int delta;// current altitude minus altitude at time of zero calibration 
+
 
 void setup() {
   Serial.begin(115200);
   lcd.begin(16, 2);
+
+  pinMode(zeroButton, INPUT);// 
     
   bool status = bme.begin();
 //  if (!status) {
@@ -21,8 +27,23 @@ void setup() {
   //lcd.print("Altitude");
 }
 void loop() {
-lcd.setCursor(0, 1);
+  if(digitalRead (zeroButton)==HIGH);
+  {
+    zero = bme.readPressure()/3386 + 0.77;
+  }
+  altitude = bme.readPressure()/3386 + 0.77;
+  delta = altitude - zero;
+  lcd.clear();
+  lcd.setCursor(0, 1);
 //lcd.print(bme.readPressure()/3386 + 0.77); 
-lcd.print(bme.readAltitude(1021)*3.3);
-delay(2000);                         
+lcd.print(zero);
+delay(500);                         
 }
+
+/*
+          lcd.clear();
+          lcd.print("Enter Password");
+          digitalRead(zeroButton);
+          digitalWrite(zeroButton,LOW);
+*/
+ 
