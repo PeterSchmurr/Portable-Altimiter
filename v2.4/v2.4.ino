521fc1d87cf53796163b6cb3564da9973e64db9f
Peter Schmurr <schmurrsmail@gmail.com>
2023-11-22 15:13:11 -0500

    add version 2.4 with 5 point derivative and reduced sensor precision

--- v2.4/v2.4.ino
+++ v2.4/v2.4.ino
@@ -13,7 +13,7 @@
 
 // control variables
 #define cutoff 2 // minimum altitude  change ( in feet) required for new rate calculation
-#define delayInterval 500 // interval in milliseconds between loop iterations
+#define delayInterval 1000 // interval in milliseconds between loop iterations
 
 #define OLED_ADDR   0x3C
 #define BME_280_ADDR 0x77
@@ -23,15 +23,22 @@ Adafruit_BME280 bme;
 
 const int zeroButton = 16;// input for resetting display to 0
 float altitude; // current reading from bme280
+float pressure;
 float zero =0; // altitude at time of zero calibration 
 float delta;// current altitude minus altitude at time of zero calibration 
 float reading1;
 float reading2;
+float reading3;
+float reading4;
+float reading5;
+float reading6;
+float reading7;
+float reading8;
+
 int sn=1;
 float r;
 
 void setup() {
-  
   Serial.begin(115200);
   pinMode(zeroButton,OUTPUT);  
   bool sensorStatus = bme.begin(BME_280_ADDR);
@@ -67,25 +74,24 @@ void loop() {
     zero =  bme.readAltitude(1013.25)*3.28;
     digitalWrite (zeroButton,LOW);
   }
-  altitude = bme.readAltitude(1013.25)*3.28;
+
+  pressure = bme.readPressure();
+  pressure = lround(pressure)-int(lround(pressure)) % 10;
+  altitude = pressure*(-0.278) + 28160;
   
-  // TO DO: return average of last 5 rate calculations
+  
    //r=averageRate();
   
-  collectReadings(altitude);
-  if (sn==3)
-  { bool intensityCheck = (abs (reading2 - reading1)<cutoff);
-  if(intensityCheck)
-    {
-      sn=1;
-      loop();
-    }
-    
-    
-    r = float(rate(reading2,reading1));
+  collectFiveReadings(altitude);
+  if (sn==6)
+  {
+      
+       r = (derivativeEstimateFiveReadings(reading1,reading2,reading4,reading5));
     Serial.println("rate   " + String(r));
     sn=1;
-  }
+    }
+
+  
   //Serial.println("Serial number = " + String(sn));
 
   delta = altitude - zero;
@@ -104,36 +110,51 @@ void loop() {
 delay(delayInterval);                       
 }
 
-
-//consumes two altitude readings and returns the difference between the two
-float rate(float altitude1, float altitude2)
-{
-  Serial.println("altitude1 =" + String(reading1));
-  Serial.println("altitude2 =" + String(reading2));
+	 float derivativeEstimateFiveReadings(float x_2,float x_1, float x1,float x2)
+	{
+		float rate = 0.083333333*(x_2 );
+		rate = rate - 0.66666666*(x_1);
+		rate = rate +0.66666666*(x1);
+		rate = rate -0.083333333* (x2) ;
+		
+		rate = rate*(1000.00/delayInterval);
+		
+		return round(rate);
+	}
   
-  return (altitude1 - altitude2)*60000/delayInterval;
-  
-}
 
-// collects two altitude readings in sequence and assigns them to global variables reading1 and reading2
-void collectReadings(float reading){
 
-  switch(sn){
-    case 1:
-      reading1 = reading;
-      Serial.println("reading1 = "+String(reading1));
-      sn = 2;
-      //Serial.println("Reached case 1 Serial number = " + String (sn));
-      break;
-    case 2:
-      reading2 = reading;
-      Serial.println("reading2 = "+String(reading2));
-      sn = 3;
-      
-    break;
-    default:  ;
-         
-  }
-}
+
+	// collects five altitude readings in sequence and assigns them to global variables reading1 through reading5
+	void collectFiveReadings(double reading){
+
+	  switch(sn){
+	    case 1:
+	    	reading1 = reading;
+	    	Serial.println("reading1 = "+String(reading1));
+	    	sn = 2;
+	    	break;
+	    case 2:
+	    	reading2 = reading;
+	    	Serial.println("reading2 = "+String(reading2));
+	    	sn = 3;
+	    	break;
+	    case 3:
+		      reading3 = reading;
+		      Serial.println("reading3 = "+String(reading3));
+		      sn = 4;
+		      break;
+	    case 4:
+		      reading4 = reading;
+		      Serial.println("reading4 = "+String(reading4));
+		      sn = 5;
+		      break;
+	    case 5:
+		      reading5 = reading;
+		      Serial.println("reading5 = "+String(reading5));
+		      sn = 6;
+		      
+	  }
+	}
 
   
